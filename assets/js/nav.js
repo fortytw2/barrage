13816db08aa11ba0327fa83cecf5ec0e6d58ab0c
fortytw2 <fortytw2@gmail.com>
2015-03-05 14:22:18 -0500

    add new logo

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -7,7 +7,7 @@ module.exports.controller = function() {
 module.exports.view = function(ctrl) {
   return <nav class="nav" tabindex="-1">
     <div class="container">
-      <a href="/?/" class="pagename current">Barrage</a>
+      <a href="/?/" class="pagename current"><img src="/img/banner.png" style="display:inline;"></img></a>
       <div class="right">
         <a href="/?/settings">Settings</a>
         <a href="https://github.com/fortytw2/barrage"><span class="octicon octicon-mark-github"></span></a>
